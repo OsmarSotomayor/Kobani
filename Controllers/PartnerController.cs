70e7801d348cb6c0a9a4330480beb6c735100732
Oscar Omar Flores Sotomayor <oscaromarfloressotomayor@gmail.com>
2024-05-12 19:06:51 -0600

    Primer Commit

---
+++ Controllers/PartnerController.cs
@@ -0,0 +1,62 @@
+﻿using Azure.Core;
+using Core.Entities;
+using Core.Interfaces;
+using Infraestructure.Repositori;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Text.RegularExpressions;
+
+namespace Kobani.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PartnerController: ControllerBase
+    {
+        private readonly IPartnerRepository partnerRepository;
+        private readonly ILogger<PartnerRepository> logger;
+
+        public PartnerController(IPartnerRepository partnerRepository, ILogger<PartnerRepository> logger) 
+        {
+            this.partnerRepository = partnerRepository;
+            this.logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IReadOnlyList<Partner>>> getPartners()
+        {
+            try
+            {
+                logger.LogInformation("getting the partners");
+                return Ok(await partnerRepository.getPartnersAsync());
+            }
+            catch (Exception ex)
+            {
+                logger.LogWarning(ex + "An error occurred while fetching partners");
+                return NotFound();
+            }
+            
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Partner>> getPartner(int id)
+        {
+            try{
+
+                var partner = await partnerRepository.getPartnerByIdAsync(id);
+                if(partner == null)
+                {
+                    logger.LogWarning("Partner was no found with id " + id);
+                    return NotFound();
+                }
+                return Ok(partner);
+
+            }catch(Exception ex)
+            {
+                logger.LogWarning(ex + "An error occurred while fetching partner with id  "+ id);
+                return NotFound();
+            }           
+        }
+       
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,35 @@
+using Core.Interfaces;
+using Infraestructure;
+using Infraestructure.Repositori;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IPartnerRepository, PartnerRepository>();
+builder.Services.AddDbContext<KobaniContext>(options => 
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("KobaniConnection"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
